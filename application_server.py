12cc7e191c3ac7d3afa1477fb2eb4b57eeb6ba55
Debian <admin@ip-172-31-12-166.eu-central-1.compute.internal>
2021-06-10 11:11:58 +0200

    separation of the fuota functions

--- application_server.py
+++ application_server.py
@@ -21,6 +21,8 @@ from google.protobuf import duration_pb2
 
 from Crypto.Cipher import AES
 
+from lorawan_deployement import *
+
 #DO DEBUG PRINT ?
 debug = 0
 
@@ -128,69 +130,7 @@ def update_device_list(dev_eui,data):
     print("-> After : ", end="")
     print(device_list)
 
-class LoraWanDeployment() :
-  deploymentData = {}
 
-  def update_deployment(self,dev_eui_list, binaryPath, key):
-    self.deploymentData.update({
-      'dev_eui_list' : dev_eui_list,
-      'binary' : binaryPath,
-      'genAppKey' : key
-    })
-    print("stored deployment data")
-    print(self.deploymentData)
-
-  def GetMcRootKeyFromGenAppKey(self,key):
-    #takes key argument as hex string, and returns a bytes array
-    cipher = AES.new(bytes.fromhex(key), AES.MODE_ECB)
-    ciphertext = cipher.encrypt(bytes(16))
-    return ciphertext
-
-  def start_deployment(self):
-    print("to use deployment data : ")
-    print(self.deploymentData)
-    # opens a file
-    #file_data = open("bin/patch_signed.bin",'rb')
-    file_data = open(self.deploymentData['binary'],'rb')
-    data = file_data.read()
-    # end of file
-
-    # Creates a device and a deployement
-    main_deploy = fuota.Deployment()
-    main_deploy.application_id = 1
-
-    for dev_eui in self.deploymentData['dev_eui_list'] :
-      device_a = fuota.DeploymentDevice()
-      device_a.dev_eui = bytes.fromhex(dev_eui)
-      device_a.mc_root_key = self.GetMcRootKeyFromGenAppKey(key = self.deploymentData['genAppKey'])
-      if debug == 1 : 
-        print("-> device to add to deployement ", end="")
-        print(device_a)
-
-      main_deploy.devices.extend([device_a])
-
-    main_deploy.multicast_group_type  = fuota.MulticastGroupType.CLASS_C
-    main_deploy.multicast_dr = 5
-    main_deploy.multicast_frequency = 868100000
-    main_deploy.multicast_group_id = 0
-    main_deploy.multicast_timeout = 40
-    main_deploy.unicast_timeout.FromSeconds(150) # FromString('25') #FromSeconds(25)
-    main_deploy.unicast_attempt_count = 5
-    main_deploy.payload = data
-    main_deploy.fragmentation_fragment_size = 200
-    main_deploy.fragmentation_redundancy = 10
-    main_deploy.fragmentation_session_index = 0
-    main_deploy.fragmentation_matrix = 0
-    main_deploy.fragmentation_block_ack_delay = 1
-    main_deploy.fragmentation_descriptor = bytes([0, 0, 0, 0])
-
-    print("deployement prepared")
-
-    client_fuota.CreateDeployment(
-      fuota.CreateDeploymentRequest(deployment = main_deploy)
-      )
-    print("deployement sent")
-    # end of deployement
 
 class LoraWanStatusVersion() :
   global device_list
@@ -626,7 +566,7 @@ class Handler(BaseHTTPRequestHandler):
 
 httpd = HTTPServer(('', 8091), Handler)
 StatusVersion = LoraWanStatusVersion()
-LWDeployment = LoraWanDeployment()
+LWDeployment = lorawan_deployement()
 #StatusVersion.send(dev_eui = (bytes([0x00, 0x80, 0x00, 0x00, 0x04, 0x00, 0x6a, 0xaa]).hex()),port = 111,payload = bytes([1, 2, 3]))
 httpd.serve_forever()
 
